af691e28bf91cfb6179329fdf0fc981b8512d178
Guilherme Henrique Taira <guilhermeht1906@gmail.com>
2025-01-21 00:00:50 -0300

    Cookies e Storage

--- frontend/src/services/auth/authService.js
+++ frontend/src/services/auth/authService.js
@@ -1,15 +1,16 @@
-import { HttpClient } from '../../infra/HttpClient/HttpClient';
+import { HttpClient } from "../../infra/HttpClient/HttpClient";
+import { tokenService } from "./tokenService";
 
 export const authService = {
   async login({ username, password }) {
     return HttpClient(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login`, {
-      method: 'POST',
-      body: { username, password }
-    })
-    .then(async (respostaDoServidor) => {
-      if(!respostaDoServidor.ok) throw new Error('Usuário ou senha inválidos!')
+      method: "POST",
+      body: { username, password },
+    }).then(async (respostaDoServidor) => {
+      if (!respostaDoServidor.ok)
+        throw new Error("Usuário ou senha inválidos!");
       const body = respostaDoServidor.body;
-      console.log(body);
-    })
-  }  
+      tokenService.save(body.data.access_token);
+    });
+  },
 };
