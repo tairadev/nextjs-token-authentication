047dccf8154918b27a3c9edd1d06f3fd7f3d96dc
Guilherme Henrique Taira <53182990+tairadev@users.noreply.github.com>
2025-01-22 15:50:58 -0300

    Módulo 4 finalizado

--- frontend/src/infra/HttpClient/HttpClient.js
+++ frontend/src/infra/HttpClient/HttpClient.js
@@ -1,10 +1,13 @@
 // Arquitetura Hexagonal
+import nookies from "nookies";
+import { tokenService } from "../../services/auth/tokenService";
+
 // Ports & Adapters
 export async function HttpClient(fetchUrl, fetchOptions) {
   const options = {
     ...fetchOptions,
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       ...fetchOptions.headers,
     },
     body: fetchOptions.body ? JSON.stringify(fetchOptions.body) : null,
@@ -16,6 +19,54 @@ export async function HttpClient(fetchUrl, fetchOptions) {
         status: respostaDoServidor.status,
         statusText: respostaDoServidor.statusText,
         body: await respostaDoServidor.json(),
+      };
+    })
+    .then(async (response) => {
+      if (!fetchOptions.refresh) return response;
+      if (response.status !== 401) return response;
+
+      try {
+        const isServer = Boolean(fetchOptions?.ctx);
+        const currentRefreshToken =
+          fetchOptions?.ctx?.req?.cookies["REFRESH_TOKEN_NAME"];
+
+        const refreshResponse = await HttpClient(
+          "http://localhost:3000/api/refresh",
+          {
+            method: isServer ? "PUT" : "GET",
+            body: isServer ? { refresh_token: currentRefreshToken } : undefined,
+          }
+        );
+
+        const newAccessToken = refreshResponse.body.data.access_token;
+        const newRefreshToken = refreshResponse.body.data.refresh_token;
+
+        console.log("oi");
+
+        if (isServer) {
+          nookies.set(fetchOptions.ctx, "REFRESH_TOKEN_NAME", newRefreshToken, {
+            httpOnly: true,
+            sameSite: "lax",
+            path: "/",
+          });
+        }
+
+        tokenService.save(newAccessToken);
+
+        const retryResponse = await HttpClient(fetchUrl, {
+          ...options,
+          refresh: false,
+          headers: {
+            Authorization: `Bearer ${newAccessToken}`,
+          },
+        });
+
+        console.log("retryResponse", retryResponse);
+
+        return retryResponse;
+      } catch (err) {
+        console.error(err);
+        return response;
       }
     });
 }
--- frontend/src/services/auth/authService.js
+++ frontend/src/services/auth/authService.js
@@ -6,12 +6,27 @@ export const authService = {
     return HttpClient(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login`, {
       method: "POST",
       body: { username, password },
-    }).then(async (respostaDoServidor) => {
-      if (!respostaDoServidor.ok)
-        throw new Error("Usuário ou senha inválidos!");
-      const body = respostaDoServidor.body;
-      tokenService.save(body.data.access_token);
-    });
+    })
+      .then(async (respostaDoServidor) => {
+        if (!respostaDoServidor.ok)
+          throw new Error("Usuário ou senha inválidos!");
+        const body = respostaDoServidor.body;
+        tokenService.save(body.data.access_token);
+
+        return body;
+      })
+      .then(async ({ data }) => {
+        const { refresh_token } = data;
+
+        const response = await HttpClient("/api/refresh", {
+          method: "POST",
+          body: {
+            refresh_token,
+          },
+        });
+
+        console.log(response);
+      });
   },
   async getSession(ctx = null) {
     const token = tokenService.get(ctx);
@@ -21,6 +36,8 @@ export const authService = {
       headers: {
         Authorization: `Bearer ${token}`,
       },
+      refresh: true,
+      ctx,
     }).then((response) => {
       if (!response.ok) throw new Error("Não autorizado!");
       return response.body.data;
