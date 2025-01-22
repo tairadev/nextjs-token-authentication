c879e01c0c60c70b65a1c295a609c8f9cae8c43e
Guilherme Henrique Taira <53182990+tairadev@users.noreply.github.com>
2025-01-22 12:37:12 -0300

    Sessão via Server Side Render

--- frontend/pages/auth-page-ssr.jsx
+++ frontend/pages/auth-page-ssr.jsx
@@ -1,14 +1,21 @@
+import { withSession } from "../src/services/auth/session";
+
 function AuthPageSSR(props) {
   return (
     <div>
-      <h1>
-        Auth Page Server Side Render
-      </h1>
-      <pre>
-        {JSON.stringify(props, null, 2)}
-      </pre> 
+      <h1>Auth Page Server Side Render</h1>
+      <pre>{JSON.stringify(props, null, 2)}</pre>
     </div>
-  )
+  );
 }
+
+// Decorator pattern
+export const getServerSideProps = withSession((ctx) => {
+  return {
+    props: {
+      session: ctx.req.session,
+    },
+  };
+});
 
 export default AuthPageSSR;
---
+++ frontend/src/services/auth/session.js
@@ -0,0 +1,24 @@
+import { authService } from "./authService";
+
+export function withSession(func) {
+  return async (ctx) => {
+    try {
+      const session = await authService.getSession(ctx);
+      const modifiedContext = {
+        ...ctx,
+        req: {
+          ...ctx.req,
+          session,
+        },
+      };
+      return func(modifiedContext);
+    } catch (err) {
+      return {
+        redirect: {
+          permanent: false,
+          destination: "/?error=401",
+        },
+      };
+    }
+  };
+}
